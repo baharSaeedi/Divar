707491381509ec1ba38f3574a4910d9014cb538a
baharSaeedi <b.saeedi.ha@gmail.com>
2023-11-26 16:53:03 +0330

    auth swagger

--- src/modules/auth/auth.swagger.js
+++ src/modules/auth/auth.swagger.js
@@ -3,3 +3,20 @@
  * tags:
  *  name: Auth
  */
+
+/**
+ * /auth/sendOTP
+ * POST:
+ *  summary : send otp code for user authorization
+ *  tags:
+ *      - Auth
+ *  requestBody:
+ *       content:
+ *              application/x-www-url-encoded
+ *          schema:
+ *              $ref : '#/components/schemas/sendOTP
+ *  responses:
+ *       200:
+ *              description :  success
+ *
+ */
