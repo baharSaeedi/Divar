f81d1f1fb1ad516bd4691a7df3aab66fa95072af
baharSaeedi <b.saeedi.ha@gmail.com>
2023-11-26 16:11:56 +0330

    main routes

---
+++ src/app.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const authRoutes = require("./modules/auth/auth.routes");
+
+const router = Router;
+
+router.use("/auth", authRoutes);
+
+module.exports = {
+  routes: router,
+};
